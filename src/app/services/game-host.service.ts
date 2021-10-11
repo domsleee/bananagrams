c269f6c33f6a143778d2a57c004dad9fedb016b3
Dom <domslee97@gmail.com>
2021-10-11 11:54:58 +1100

    add randomisation

--- src/app/services/game-host.service.ts
+++ src/app/services/game-host.service.ts
@@ -147,6 +147,15 @@ export class GameHostService {
 
   private addLetters(letters: Iterable<Letter>) {
     this.state.letters.concat([...letters]);
+    this.shuffleArray(this.state.letters);
+  }
+
+  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  private shuffleArray(array: Array<any>) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
   }
 
   private takeLetters(n: number) {
