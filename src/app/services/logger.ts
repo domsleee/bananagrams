240cce60997cb6f489c6b3f6710d206c877dcd55
Dom <domslee97@gmail.com>
2022-03-08 23:23:08 +1100

    ability to download logs

--- src/app/services/logger.ts
+++ src/app/services/logger.ts
@@ -1,16 +1,68 @@
 import * as log from 'loglevel';
 import * as prefix from 'loglevel-plugin-prefix';
 
+const TXT_LEVEL: log.LogLevelDesc = 'DEBUG';
+const LOG_LEVEL: log.LogLevelDesc = 'INFO';
+
 prefix.reg(log);
-prefix.apply(log, {
-  format(level: string, name: any, timestamp: any) {
-    return `[${timestamp}] ${level} ${name}:`;
-  }
-});
+prefix.apply(log, { format });
+
+function format(level: string, name: any, timestamp: any) {
+  return `[${timestamp}] ${level} ${name}:`;
+}
 
 export function getLogger(service: string) {
   const ret = log.getLogger(service);
-  ret.setLevel('INFO');
+  ret.setLevel(TXT_LEVEL);
+
   //if (service === 'peer-to-peer.service') ret.setLevel('DEBUG');
   return ret;
 }
+
+const allLogs = new Array<string>();
+const originalFactory = log.methodFactory;
+const logMethods = [
+  "trace",
+  "debug",
+  "info",
+  "warn",
+  "error"
+];
+
+const myMethodFactory = (methodName: string, logLevel: log.LogLevelNumbers, loggerName: any) => {
+  const rawMethod = originalFactory(methodName, logLevel, loggerName);
+
+  return function (...args: any) {
+    const currDate = (new Date()).toTimeString().split(' ')[0]
+    allLogs.push(`[${currDate}]: ${logMethods[logLevel].toUpperCase()} ${args.map(t => JSON.stringify(t)).join(' ')}`);
+    
+    if (logLevel >= log.levels[LOG_LEVEL]) {
+      rawMethod(...args);
+    }
+  };
+};
+log.setLevel(log.getLevel()); // Be sure to call setLevel method in order to apply plugin
+
+function attachMethodFactory() {
+  if (log.methodFactory !== myMethodFactory) {
+    // @ts-ignore: read-only
+    log.methodFactory = myMethodFactory;
+  }  
+}
+
+export function downloadLogs() {
+  downloadText(`${(new Date()).toISOString()}.txt`, allLogs.join('\n'));
+}
+
+// https://stackoverflow.com/questions/24898044/is-possible-to-save-javascript-variable-as-file
+function downloadText(filename: string, textToSave: string) {
+  var hiddenElement = document.createElement('a');
+
+  hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+  hiddenElement.target = '_blank';
+  hiddenElement.download = filename;
+  hiddenElement.click();
+}
+
+window['downloadLogs'] = downloadLogs;
+attachMethodFactory();
