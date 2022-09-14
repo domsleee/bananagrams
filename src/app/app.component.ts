aa31152915b34f7934d56dc63cb9741b9704e83b
Dom Slee <domslee97@gmail.com>
2022-09-15 08:21:36 +1000

    improve appearance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bananagrams';
+  myTitle = "BGRAMS".split("");
+  version = packageJson.version;
 }
