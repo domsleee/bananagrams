a572c1baab838f8b091bf7f3cd8b904c616925ab
Dom Slee <domslee97@gmail.com>
2022-09-23 00:08:16 +1000

    improve page transition animation performance + feel

--- src/app/animations/fade.animation.ts
+++ src/app/animations/fade.animation.ts
@@ -6,16 +6,16 @@ export const fadeAnimation =
       query(':enter', [
         style({ opacity: 0 }),
       ]),
-
-      query(':leave', [
-        sequence([
-          animate('0.1s', style({opacity: 0})),
-          style({display: 'none', position: 'absolute'})
+      sequence([
+        query(':leave', [
+          sequence([
+            animate('0.1s', style({opacity: 0})),
+            style({position: 'absolute', visibility: 'hidden'})
+          ])
+        ], { optional: true }),
+        query(':enter', [
+          animate('0.1s', style({ opacity: 1 }))
         ])
-      ], { optional: true }),
-
-      query(':enter', [
-        animate('0.1s 0.1s', style({ opacity: 1 }))
       ])
     ])
   ]);
